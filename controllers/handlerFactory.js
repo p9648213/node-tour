52d5b1b7596676c24fc9c0d81fb8a0d4505cc9bc
p9648213 <p9648213750912@gmail.com>
2023-06-04 07:47:18 +0700

    first commit

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,110 @@
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, populateOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+
+    if (populateOptions) {
+      query = query.populate(populateOptions);
+    }
+
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res) => {
+    // To allow for nested Get reviews on tour
+    let filter = {};
+    if (req.params.tourId) {
+      filter = { tour: req.params.tourId };
+    }
+
+    //EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    // Clone the query object to preserve the original query
+    const countQuery = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields();
+
+    // Get the total number of documents after filter and sort
+    const totalDocsBeforePaginate = await countQuery.query.countDocuments();
+
+    // Examise the read status
+    // const doc = await features.query.explain();
+    const doc = await features.query;
+
+    //SEND RESPONSE
+    res.status(200).json({
+      status: 'sucess',
+      requestedAt: req.requestTime,
+      totalDocsBeforePaginate: totalDocsBeforePaginate,
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
